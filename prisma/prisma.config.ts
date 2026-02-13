870a11a67b54b9a007d50bdfc58545a3ea6cfa7c
ibrahimahmed <ibrahimahmed246666@gmail.com>
2026-02-13 04:13:44 +0200

    Update prisma config and schema

--- prisma/prisma.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'prisma/config';
-
-export default defineConfig({
-  schema: 'prisma/schema.prisma',   // مسار schema
-  datasource: {
-    url: process.env.DATABASE_URL!, // بيجيب الـ URL من .env
-  },
-});
